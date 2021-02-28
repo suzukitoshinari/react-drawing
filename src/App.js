2316525d935fa061be5c24baa9df428818a89cce
suzukitoshinari <ay658mymt@gmail.com>
2021-02-28 22:04:44 +0900

    update

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
-  const [show, setShow] = useState(false);
-  // const [color, setColor] = useState('#f000000');
+  const [showFill, setShowFill] = useState(false);
+  const [showPen, setShowPen] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;  
@@ -20,7 +20,7 @@ function App() {
     context.scale(2,2);
     context.lineCap = 'round';
     context.strokeStyle = 'black';
-    context.lineWidth = 5
+    context.lineWidth = 5;
     contextRef.current = context;
   }, [])
 
@@ -48,42 +48,88 @@ function App() {
   const clearCanvas = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d')
-    context.fillStyle = 'white'
-    context.fillRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
   }
 
   const eraser = () => {
     const canvas = canvasRef.current;  
     const context = canvas.getContext('2d');
-    context.strokeStyle = 'white'
+    context.strokeStyle = 'white';
   }
 
-  const fill = () => {
+  const red = () => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d')
-    context.fillStyle = 'red'
+    const context = canvas.getContext('2d');
+    context.fillStyle = 'red';
     context.fill();
     context.stroke();
   }
 
-  const Paper = () => {
-    if(show) {
+  const green = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.fillStyle = 'green';
+    context.fill();
+    context.stroke();
+  }
+
+  const blue = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d')
+    context.fillStyle = 'blue'
+    context.fill();
+    context.stroke();
+  }
+
+  const Fill = () => {
+    if(showFill) {
       return (
         <div className='paper'>
-            <button className='close' onClick={() => setShow(false)}>✖️</button>
-            <button className='color' onClick={fill}></button>
+            <button className='close' onClick={() => setShowFill(false)}>✖️</button>
+            <button className='color1' onClick={red}></button>
+            <button className='color2' onClick={green}></button>
+            <button className='color3' onClick={blue}></button>
         </div>
       )
     } else {
       return null;
     }
-}
+  }
 
-  const pen = () => {
+  const Pen = () => {
+    if(showPen) {
+      return (
+        <div className='paper'>
+            <button className='close' onClick={() => setShowPen(false)}>✖️</button>
+            <button className='color1' onClick={pen1}></button>
+            <button className='color2' onClick={pen2}></button>
+            <button className='color3' onClick={pen3}></button>
+        </div>
+      )
+    } else {
+      return null;
+    }
+  }
+
+  const pen1 = () => {
     const canvas = canvasRef.current;  
     const context = canvas.getContext('2d');
-    context.strokeStyle = 'black'
+    context.strokeStyle = 'red'
   }
+
+  const pen2 = () => {
+    const canvas = canvasRef.current;  
+    const context = canvas.getContext('2d');
+    context.strokeStyle = 'green'
+  }
+
+  const pen3 = () => {
+    const canvas = canvasRef.current;  
+    const context = canvas.getContext('2d');
+    context.strokeStyle = 'blue'
+  }
+
 
   // const setColor = () => {
   //   setColor();
@@ -96,10 +142,11 @@ function App() {
       </header>
       <div className='label'>
         <button className='btn' onClick={clearCanvas}>白紙にする</button>
-        <button className='btn' onClick={pen}>ペン</button>
+        <button className='btn' onClick={() => setShowPen(true)}>ペン</button>
+        <Pen />
         <button className='btn' onClick={eraser}>消しゴム</button>
-          <button className='btn_fill' onClick={() => setShow(true)}>塗り潰し</button>
-          <Paper />
+        <button className='btn_fill' onClick={() => setShowFill(true)}>塗り潰し</button>
+        <Fill />
       </div>
       <div className='main_canvas'>
         <canvas 
